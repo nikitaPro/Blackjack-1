af12dbb7120cc892738c37052a48d2bd60530000
Nikita <nikita_prokopenko@yahoo.com>
2015-09-28 17:29:27 +0300

    Add arhitype

---
+++ target/classes/first/HelloBlackJack.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package first;
+
+/**
+ * @author NikitaNB
+ *
+ */
+public class HelloBlackJack {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Black Jack! Lets Play!");
+	}
+
+}
