8e58b85ea26009e99c045179229816d959fd7be3
Nikita <nikita_prokopenko@yahoo.com>
2015-10-17 20:38:32 +0300

    karaf working Hellow Worldgit status!

---
+++ com.sigma.database/src/main/java/com/javaworld/sample/helloservice/HelloServiceFactory.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.javaworld.sample.helloservice;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.ServiceFactory;
+import org.osgi.framework.ServiceRegistration;
+
+import com.javaworld.sample.service.impl.HelloServiceImpl;
+
+/**
+ * @author NikitaNB
+ *
+ */
+	public class HelloServiceFactory implements ServiceFactory{
+	    private int usageCounter = 0;
+	    public Object getService(Bundle bundle, ServiceRegistration registration) {
+	        System.out.println("Create object of HelloService for " + bundle.getSymbolicName());
+	        usageCounter++;
+	        System.out.println("Number of bundles using service " + usageCounter);
+	        HelloService helloService = new HelloServiceImpl();
+	        return helloService;
+	    }
+	    public void ungetService(Bundle bundle, ServiceRegistration registration, Object service) {
+	        System.out.println("Release object of HelloService for " + bundle.getSymbolicName());
+	        usageCounter--;
+	        System.out.println("Number of bundles using service " + usageCounter);
+	    }
+	}
+
---
+++ com.sigma.database/src/main/java/com/javaworld/sample/service/impl/HelloServiceImpl.java
@@ -0,0 +1,12 @@
+package com.javaworld.sample.service.impl;
+
+import com.javaworld.sample.helloservice.HelloService;
+
+
+public class HelloServiceImpl implements HelloService{
+
+	    public String sayHello() {
+	        System.out.println("Inside HelloServiceImple.sayHello()");
+	        return "Say Hello yo!!";
+	    }
+	}
